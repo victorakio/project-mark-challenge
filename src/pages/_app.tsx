d975741332e88eae75cd5fccf0649f9585d165d7
Victor Akio <victorakioz@gmail.com>
2023-08-10 15:06:36 -0300

    chore: initial project setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { CSSReset } from '@stlyes/reset';
+import Head from 'next/head';
+import { StyleSheetManager } from 'styled-components';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <StyleSheetManager enableVendorPrefixes>
+      <Head>
+        <title>Project Mark Challenge</title>
+
+        <meta name="description" content="Frontend code challenge" />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <link rel="icon" href="/favicon.ico" />
+
+        <link rel="shortcut icon" href="/icon-512.png" />
+
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </Head>
+
+      <CSSReset />
+
+      <Component {...pageProps} />
+    </StyleSheetManager>
+  );
 }
